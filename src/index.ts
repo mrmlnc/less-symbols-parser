25afe82c62a89d7cfaf515c3ad265ada42d05755
Denis Malinochkin <dmalinochkin@rambler.ru>
2016-10-21 03:29:12 +0300

    init :panda_face:

---
+++ src/index.ts
@@ -0,0 +1,211 @@
+'use strict';
+
+import tokenizer from './tokenizer';
+
+export interface IVariable {
+	name: string;
+	value: string;
+	line: number;
+	column: number;
+}
+
+export interface IImport {
+	filepath: string;
+	modes: string[];
+	dynamic: boolean;
+}
+
+export interface IMixin {
+	name: string;
+	parameters: IVariable[];
+	line: number;
+	column: number;
+}
+
+function makeMixinParameter(text: string, line: number, column: number): IVariable[] {
+	if (text === '()') {
+		return [];
+	}
+
+	const params = text.slice(1, text.length - 1).split(/,\s*|;\s*/);
+	const variables = params.map((x) => {
+		const stat = x.match(/(@[\w-]+)(?:\s*:\s*(.*))?/);
+
+		return <IVariable>{
+			name: stat[1],
+			value: stat[2] || null,
+			line,
+			column
+		};
+	});
+
+	return variables;
+}
+
+function parse(text: string) {
+	const tokens = tokenizer(text);
+
+	let variables: IVariable[] = [];
+	let mixins: IMixin[] = [];
+	let imports: IImport[] = [];
+
+	let token;
+	let pos = 0;
+
+	let line = 1;
+
+	const length = tokens.length;
+
+	while (pos < length) {
+		token = tokens[pos];
+
+		if (token[0] === 'at-word' && token[1] === '@import') { // Import's
+			pos++;
+
+			let str = '';
+			while (true) {
+				token = tokens[pos];
+				if (token[0] === ';') {
+					break;
+				}
+				str += token[1];
+				pos++;
+			}
+
+			const stat = str.match(/(?:\(([\w-,\s]+)\))?\s"(.*)"/);
+
+			imports.push({
+				filepath: stat[2],
+				modes: stat[1] ? stat[1].split(/,\s*/) : [],
+				dynamic: /[@{}\*]/.test(stat[2])
+			});
+		} else if (token[0] === 'at-word' && token[1].endsWith(':')) { // Variables
+			line = token[2];
+			pos++;
+
+			const name = token[1].slice(0, -1);
+			const column = token[3];
+
+			let value = '';
+			while (tokens[pos][0] !== ';') {
+				token = tokens[pos];
+
+				if (token[0] === '{') {
+					let ruleset = 1;
+
+					value += '{';
+
+					pos++;
+					while (true) {
+						token = tokens[pos];
+						if (token[0] === ';' && ruleset === 0) {
+							break;
+						} else if (token[0] === '{') {
+							ruleset++;
+						} else if (token[0] === '}') {
+							ruleset--;
+
+							if (ruleset === 0) {
+								value += '}';
+								break;
+							}
+						}
+
+						value += token[1].replace(/\t/g, '');
+						pos++;
+					}
+				} else if (token[0] === 'word' || token[0] === 'at-word' || token[0] === 'string' || token[0] === 'space') {
+					value += token[1];
+				}
+				pos++;
+			}
+
+			variables.push({
+				name,
+				value: value.trim(),
+				line,
+				column
+			});
+		} else if (token[0] === 'word' && (token[1].startsWith('.') || token[1].startsWith('#'))) { // Potential Mixin
+			const column = token[3];
+			let paramsColumn = column;
+
+			line = token[2];
+
+			let name = '';
+			while (true) {
+				token = tokens[pos];
+				if (token[0] === ':' || token[0] === 'brackets' || token[0] === '(' || token[0] === '{') {
+					break;
+				}
+				name += token[1];
+				pos++;
+			}
+
+			let params = '';
+			if (token[0] === 'brackets') {
+				paramsColumn = token[3];
+				params = token[1];
+			} else if (token[0] === '(') {
+				paramsColumn = token[3];
+				pos++;
+				while (true) {
+					token = tokens[pos];
+					if (token[0] === ')') {
+						break;
+					}
+
+					params += token[1];
+					pos++;
+				}
+
+				params = `(${params})`;
+			}
+
+			if (tokens[pos + 1][0] === ';' || tokens[pos + 2][0] === ';') {
+				pos++;
+				continue;
+			}
+
+			if (name && params) {
+				mixins.push({
+					name: name.trim(),
+					parameters: makeMixinParameter(params, line, paramsColumn),
+					line,
+					column
+				});
+			} else {
+				pos--;
+			}
+		} else if (token[0] === '{') { // Ruleset
+			let ruleset = 1;
+
+			pos++;
+			while (true) {
+				token = tokens[pos];
+				if (ruleset === 0) {
+					break;
+				} else if (token[0] === '{') {
+					ruleset++;
+				} else if (token[0] === '}') {
+					ruleset--;
+				}
+				pos++;
+			}
+		}
+
+		pos++;
+	}
+
+	return {
+		variables,
+		mixins,
+		imports
+	};
+}
+
+export function getParser() {
+	return {
+		parse: (text: string) => parse(text)
+	};
+}
---
+++ src/tokenizer.ts
@@ -0,0 +1,303 @@
+'use strict';
+
+// The MIT License (MIT)
+
+// Copyright 2013 Andrey Sitnik <andrey@sitnik.ru>
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+// ## CHANGES
+//   * `//` - comment
+//   * Remove `options.ignore`
+
+const SINGLE_QUOTE = '\''.charCodeAt(0);
+const DOUBLE_QUOTE = '"'.charCodeAt(0);
+const BACKSLASH = '\\'.charCodeAt(0);
+const SLASH = '/'.charCodeAt(0);
+const NEWLINE = '\n'.charCodeAt(0);
+const SPACE = ' '.charCodeAt(0);
+const FEED = '\f'.charCodeAt(0);
+const TAB = '\t'.charCodeAt(0);
+const CR = '\r'.charCodeAt(0);
+const OPEN_SQUARE = '['.charCodeAt(0);
+const CLOSE_SQUARE = ']'.charCodeAt(0);
+const OPEN_PARENTHESES = '('.charCodeAt(0);
+const CLOSE_PARENTHESES = ')'.charCodeAt(0);
+const OPEN_CURLY = '{'.charCodeAt(0);
+const CLOSE_CURLY = '}'.charCodeAt(0);
+const SEMICOLON = ';'.charCodeAt(0);
+const ASTERISK = '*'.charCodeAt(0);
+const COLON = ':'.charCodeAt(0);
+const AT = '@'.charCodeAt(0);
+
+const RE_AT_END = /[ \n\t\r\f\{\(\)'"\\;/\[\]#]/g;
+const RE_WORD_END = /[ \n\t\r\f\(\)\{\}:;@!'"\\\]\[#]|\/(?=\*)/g;
+const RE_BAD_BRACKET = /.[\\\/\("'\n]/;
+
+export default function tokenizer(text) {
+	let tokens = [];
+
+	let code, next, quote, lines, last, content, escape,
+		nextLine, nextOffset, escaped, escapePos, prev, n;
+
+	let length = text.length;
+	let offset = -1;
+	let line = 1;
+	let pos = 0;
+
+	while (pos < length) {
+		code = text.charCodeAt(pos);
+
+		if (code === NEWLINE || code === FEED ||
+			code === CR && text.charCodeAt(pos + 1) !== NEWLINE) {
+			offset = pos;
+			line += 1;
+		}
+
+		switch (code) {
+			case NEWLINE:
+			case SPACE:
+			case TAB:
+			case CR:
+			case FEED:
+				next = pos;
+				do {
+					next += 1;
+					code = text.charCodeAt(next);
+					if (code === NEWLINE) {
+						offset = next;
+						line += 1;
+					}
+				} while (code === SPACE ||
+				code === NEWLINE ||
+				code === TAB ||
+				code === CR ||
+					code === FEED);
+
+				tokens.push(['space', text.slice(pos, next)]);
+				pos = next - 1;
+				break;
+
+			case OPEN_SQUARE:
+				tokens.push(['[', '[', line, pos - offset]);
+				break;
+
+			case CLOSE_SQUARE:
+				tokens.push([']', ']', line, pos - offset]);
+				break;
+
+			case OPEN_CURLY:
+				tokens.push(['{', '{', line, pos - offset]);
+				break;
+
+			case CLOSE_CURLY:
+				tokens.push(['}', '}', line, pos - offset]);
+				break;
+
+			case COLON:
+				tokens.push([':', ':', line, pos - offset]);
+				break;
+
+			case SEMICOLON:
+				tokens.push([';', ';', line, pos - offset]);
+				break;
+
+			case OPEN_PARENTHESES:
+				prev = tokens.length ? tokens[tokens.length - 1][1] : '';
+				n = text.charCodeAt(pos + 1);
+				if (prev === 'url' && n !== SINGLE_QUOTE && n !== DOUBLE_QUOTE &&
+					n !== SPACE && n !== NEWLINE && n !== TAB &&
+					n !== FEED && n !== CR) {
+					next = pos;
+					do {
+						escaped = false;
+						next = text.indexOf(')', next + 1);
+						if (next === -1) {
+							next = pos;
+							break;
+						}
+						escapePos = next;
+						while (text.charCodeAt(escapePos - 1) === BACKSLASH) {
+							escapePos -= 1;
+							escaped = !escaped;
+						}
+					} while (escaped);
+
+					tokens.push(['brackets', text.slice(pos, next + 1),
+						line, pos - offset,
+						line, next - offset
+					]);
+					pos = next;
+
+				} else {
+					next = text.indexOf(')', pos + 1);
+					content = text.slice(pos, next + 1);
+
+					if (next === -1 || RE_BAD_BRACKET.test(content)) {
+						tokens.push(['(', '(', line, pos - offset]);
+					} else {
+						tokens.push(['brackets', content,
+							line, pos - offset,
+							line, next - offset
+						]);
+						pos = next;
+					}
+				}
+
+				break;
+
+			case CLOSE_PARENTHESES:
+				tokens.push([')', ')', line, pos - offset]);
+				break;
+
+			case SINGLE_QUOTE:
+			case DOUBLE_QUOTE:
+				quote = code === SINGLE_QUOTE ? '\'' : '"';
+				next = pos;
+				do {
+					escaped = false;
+					next = text.indexOf(quote, next + 1);
+					if (next === -1) {
+						next = pos + 1;
+						break;
+					}
+					escapePos = next;
+					while (text.charCodeAt(escapePos - 1) === BACKSLASH) {
+						escapePos -= 1;
+						escaped = !escaped;
+					}
+				} while (escaped);
+
+				content = text.slice(pos, next + 1);
+				lines = content.split('\n');
+				last = lines.length - 1;
+
+				if (last > 0) {
+					nextLine = line + last;
+					nextOffset = next - lines[last].length;
+				} else {
+					nextLine = line;
+					nextOffset = offset;
+				}
+
+				tokens.push(['string', text.slice(pos, next + 1),
+					line, pos - offset,
+					nextLine, next - nextOffset
+				]);
+
+				offset = nextOffset;
+				line = nextLine;
+				pos = next;
+				break;
+
+			case AT:
+				RE_AT_END.lastIndex = pos + 1;
+				RE_AT_END.test(text);
+				if (RE_AT_END.lastIndex === 0) {
+					next = text.length - 1;
+				} else {
+					next = RE_AT_END.lastIndex - 2;
+				}
+				tokens.push(['at-word', text.slice(pos, next + 1),
+					line, pos - offset,
+					line, next - offset
+				]);
+				pos = next;
+				break;
+
+			case BACKSLASH:
+				next = pos;
+				escape = true;
+				while (text.charCodeAt(next + 1) === BACKSLASH) {
+					next += 1;
+					escape = !escape;
+				}
+				code = text.charCodeAt(next + 1);
+				if (escape && (code !== SLASH &&
+					code !== SPACE &&
+					code !== NEWLINE &&
+					code !== TAB &&
+					code !== CR &&
+					code !== FEED)) {
+					next += 1;
+				}
+				tokens.push(['word', text.slice(pos, next + 1),
+					line, pos - offset,
+					line, next - offset
+				]);
+				pos = next;
+				break;
+
+			default:
+				const isLessComent = code === SLASH && text.charCodeAt(pos + 1) === SLASH;
+				if (isLessComent || text.charCodeAt(pos + 1) === ASTERISK) {
+					if (isLessComent) {
+						next = text.indexOf('\n', pos + 1);
+					} else {
+						next = text.indexOf('*/', pos + 2) + 1;
+					}
+
+					if (next === 0) {
+						next = text.length;
+					}
+
+					content = text.slice(pos, next + 1);
+					lines = content.split('\n');
+					last = lines.length - 1;
+
+					if (last > 0) {
+						nextLine = line + last;
+						nextOffset = next - lines[last].length;
+					} else {
+						nextLine = line;
+						nextOffset = offset;
+					}
+
+					tokens.push(['comment', content,
+						line, pos - offset,
+						nextLine, next - nextOffset
+					]);
+
+					offset = nextOffset;
+					line = nextLine;
+					pos = next;
+
+				} else {
+					RE_WORD_END.lastIndex = pos + 1;
+					RE_WORD_END.test(text);
+					if (RE_WORD_END.lastIndex === 0) {
+						next = text.length - 1;
+					} else {
+						next = RE_WORD_END.lastIndex - 2;
+					}
+
+					tokens.push(['word', text.slice(pos, next + 1),
+						line, pos - offset,
+						line, next - offset
+					]);
+					pos = next;
+				}
+
+				break;
+		}
+
+		pos++;
+	}
+
+	return tokens;
+};
